cff8c3e2110e6a70ade3110f773ab66068ba61dd
wgwz <klawlor419@gmail.com>
2018-12-30 16:28:55 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     long_description=readme + '\n\n' + history,
     author="Kyle Lawlor",
     author_email='klawlor419@gmail.com',
-    url='https://github.com/wgwz/flask_decode',
+    url='https://github.com/wgwz/flask-decode',
     packages=[
         'flask_decode',
     ],
@@ -39,12 +39,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements
