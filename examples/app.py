53ed51cd96676f0d8a54a0f8a445553e448381b3
wgwz <klawlor419@gmail.com>
2019-01-05 19:50:43 -0500

    Correct expirations and improve error handling

--- examples/app.py
+++ examples/app.py
@@ -2,12 +2,15 @@ from flask import Flask, jsonify, session, request
 from flask_cookie_decode import CookieDecode
 
 app = Flask(__name__)
-app.config.update({'SECRET_KEY': 'jlghasdghasdhgahsdg'})
+app.config.update(
+    {"SECRET_KEY": "jlghasdghasdhgahsdg", "PERMANENT_SESSION_LIFETIME": 3600}
+)
 flask_decode = CookieDecode()
 flask_decode.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    a = request.args.get('a')
-    session['a'] = a
+    a = request.args.get("a")
+    session["a"] = a
     return jsonify(dict(session))
