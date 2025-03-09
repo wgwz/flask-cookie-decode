6779d7285038436511ee9bb0f305141b6b5cfaeb
Kyle Lawlor-Bagcal <wgwz@pm.me>
2025-03-09 13:17:12 -0400

    run black

--- flask_cookie_decode/cookie_decode.py
+++ flask_cookie_decode/cookie_decode.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta, timezone 
+from datetime import datetime, timedelta, timezone
 from collections import namedtuple
 import click
 from itsdangerous.timed import TimestampSigner
